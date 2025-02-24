57a8da5b786831e69f5202bd3155251aa32ce4fa
jwkim <wer07032@naver.com>
2025-02-24 17:30:20 +0900

    git init

---
+++ spring-blog/src/main/java/com/dali186/springblog/SpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dali186.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogApplication.class, args);
+    }
+
+}
---
+++ spring-blog/src/test/java/com/dali186/springblog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dali186.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

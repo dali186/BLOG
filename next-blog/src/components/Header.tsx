ab1e76713e331c11a43f909a9c53c8b5783fddd0
jwkim <wer07032@naver.com>
2025-03-27 11:12:21 +0900

    rebase git commits

--- next-blog/src/components/Header.tsx
+++ next-blog/src/components/Header.tsx
@@ -1,23 +1,23 @@
 const Header = () => {
-
     return (
-        <header className="bg-white">
-            <div className="container mx-auto flex items-center justify-between p-4">
-            <div className="flex itmes-center">
-                <a className="gh-head-logo" href="/">
-                    <img src="/images/logo.png" alt="MainLogo" className="h-8" />
-                </a>
-            </div>
-            <div className="flex items-center">
-                <button className="gh-search gh-icon-btn" aria-label="Search this site">
-                    <svg xmlns='http://www.w3.org/2000/svg' fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke='currentColor'>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-                </button>
-            </div>
-            </div>
-        </header>
-    );
-}
+      <header className="bg-white">
+        <div className="container mx-auto flex items-center justify-between p-4">
+          {/* 로고 */}
+          <div className="flex items-center">
+            <a href="/" className="gh-head-logo">
+              <img src="/images/logo.png" alt="MainLogo" className="h-8" />
+            </a>
+          </div>
 
-export default Header;
+          <div className="text-xs sm:text-base flex items-center space-x-1">
+            <a href="/articles/write" className="text-gray-700 hover:text-blue-500 font-noto">Write</a>
+            <p>•</p>
+            <a href="/signup" className="text-gray-700 hover:text-blue-500 font-noto">Signin</a>
+          </div>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
+  

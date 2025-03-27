1cec7470b1b5641bb0ac9986a2c3ef3d06471679
jwkim <wer07032@naver.com>
2025-03-27 11:17:08 +0900

    repository rebase

--- spring-blog/src/main/java/com/dali186/springblog/domain/article/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dali186.springblog.domain.article.repository;
-
-import com.dali186.springblog.domain.article.model.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
